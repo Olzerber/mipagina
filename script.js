10523be96f83630a1f6a97b30d39ec75944ceda6
Olzerber <97409672+Olzerber@users.noreply.github.com>
2024-04-04 08:42:44 -0300

    Header desktop Listo
    Termino el header desktop, y ahora arranque el main.

--- script.js
+++ script.js
@@ -10,4 +10,39 @@ btnMobile.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     navMobile.classList.remove('show');
 });
+/*---- TOGGLE MENU ----*/
 
+/*---- Buscador ----*/
+const iconSearch = document.getElementById('icon_search');
+const inputSearch = document.querySelector('.nav_desktop_input');
+
+iconSearch.addEventListener('click',()=> {
+    inputSearch.classList.toggle('mostrar');
+});
+
+/*---- Dropdown ----*/
+const dropdown = document.getElementById('dropdown')
+const dropdownContent = document.querySelector('.dropdown_content')
+
+dropdown.addEventListener('click', () =>{
+    dropdownContent.classList.toggle('hidden');
+})
+/* ----Dropdown---- */
+
+/*----Acordion----*/
+let acordion = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acordion.length; i++) {
+acordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+/*----Acordion----*/
